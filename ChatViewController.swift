bb1263ae5f97430bf1a20214db1cc0342ad2d872
Brinder Dhaliwal <brinder.dhaliwal13@gmail.com>
2017-07-30 13:09:21 -0700

    xcode contents

---
+++ ChatViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ChatViewController.swift
+//  My Circle
+//
+//  Created by Nataliia Koldaeva on 7/29/17.
+//  Copyright © 2017 Nataliia Koldaeva. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ChatViewController: UIViewController {
+    
+//    @IBOutlet var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        // var url =
+//        
+//        let url = NSURL (string: "https://www.google.com")
+//        let request = NSURLRequest(url: url! as URL);
+//        webView.loadRequest(request as URLRequest)
+    }
+}
---
+++ ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  My Circle
+//
+//  Created by Nataliia Koldaeva on 7/29/17.
+//  Copyright © 2017 Nataliia Koldaeva. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
